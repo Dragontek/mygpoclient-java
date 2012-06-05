aeea2cc29da1dce69f891bb2bb566444cdb02f74
joshua.mondragon <joshua.mondragon@dragontek.com>
2012-06-04 21:41:56 -0600

    ADDED: Structure for maven build

---
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Global.java
@@ -0,0 +1,10 @@
+package com.dragontek.mygpoclient;
+
+public class Global {
+	public static String HOST = "gpodder.net";
+	public static int VERSION = 2;
+	public static int TOPLIST_DEFAULT = 25;
+	public static String WEBSITE = "http://www.dragontek.com/mygpoclient";
+	
+	public static String USER_AGENT = String.format("mygpoclient/%s (%s)", VERSION, WEBSITE);
+}
