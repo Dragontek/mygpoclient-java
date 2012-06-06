83c8a8dfcf3ad2d4c80b7e247a8ae8f6618f7fbe
Joshua Mondragon <joshua.mondragon@dragontek.com>
2012-06-06 09:33:26 -0600

    CHANGED: Global USER_AGENT and WEBSITE

--- mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Global.java
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Global.java
@@ -5,8 +5,8 @@ public class Global {
 	public static String HOST = "gpodder.net";
 	public static int VERSION = 2;
 	public static int TOPLIST_DEFAULT = 25;
-	public static String WEBSITE = "http://www.dragontek.com/mygpoclient";
+	public static String WEBSITE = "http://dragontek.github.com/mygpoclient-java";
 	
-	public static String USER_AGENT = String.format("mygpoclient/%s (%s)", VERSION, WEBSITE);
+	public static String USER_AGENT = String.format("mygpoclient-java/%s (%s)", VERSION, WEBSITE);
 	
 }
