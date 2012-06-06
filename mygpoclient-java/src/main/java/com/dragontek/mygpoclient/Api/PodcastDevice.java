4d083bace896515d9ece4f091fe4c830daee966d
Josh <joshua.mondragon@dragontek.com>
2012-06-06 09:15:08 -0600

    ADDED: Check for valid types

--- mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Api/PodcastDevice.java
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Api/PodcastDevice.java
@@ -17,8 +17,15 @@ public class PodcastDevice {
 	/** The number of podcasts this device is subscribed to */
 	public int _subscriptions;
 
+	private static String[] VALID_TYPES = new String[] { "desktop", "laptop", "mobile", "server", "other" };
+
 	public PodcastDevice(String deviceId, String caption, String type, int subscriptions)
 	{
+		if(!Arrays.asList(VALID_TYPES).contains(type))
+		{
+			// TODO: Throw error	
+		}
+			
 		this.deviceId = deviceId;
 		this.caption = caption;
 		this._type = type;
