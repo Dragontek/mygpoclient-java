0c14904785ddc1db6887a60bb54ce24785aca948
Josh Mondragon <joshua.mondragon@dragontek.com>
2012-06-18 22:06:25 -0600

    FIXED: Package name conventions

--- mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Main.java
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Main.java
@@ -1,5 +1,10 @@
 package com.dragontek.mygpoclient;
 
+import com.dragontek.mygpoclient.pub.PublicClient;
+import com.dragontek.mygpoclient.pub.ToplistPodcast;
+import com.dragontek.mygpoclient.simple.Podcast;
+import java.util.List;
+
 public class Main {
 
 	/**
@@ -7,7 +12,18 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("mygpoclient-java");
+		System.out.println("mygpoclient");
+		PublicClient client = new PublicClient();
+		try {
+			List<Podcast> podcasts = client.searchPodcast("linux");
+
+			for(Podcast podcast : podcasts)
+			{
+				System.out.println(podcast.title);
+			}
+		} catch(Exception e) {
+			System.out.println("ERROR");
+		}
 	}
 
 }
