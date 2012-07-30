bfccf196b40e2b539651fc3d1f0f6221f166c900
Josh Mondragon <joshua.mondragon@dragontek.com>
2012-07-30 14:48:05 -0600

    ADDED: Debug option for console output
    Used to debug HTTP calls, provides some output on the URL being called,
    authentication and response.

--- mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Global.java
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/Global.java
@@ -2,11 +2,13 @@ package com.dragontek.mygpoclient;
 
 public class Global {
 
-	public static String HOST = "gpodder.net";
+	public static boolean DEBUG = true;
+
+	public static String HOST =  "gpodder.net";
 	public static int VERSION = 2;
 	public static int TOPLIST_DEFAULT = 25;
 	public static String WEBSITE = "http://dragontek.github.com/mygpoclient-java";
 	
 	public static String USER_AGENT = String.format("mygpoclient-java/%s (%s)", VERSION, WEBSITE);
-	
+
 }
