0c6728948f0da0884e5d5ad58d07b27ec6df4d8b
Josh Mondragon <joshua.mondragon@dragontek.com>
2012-07-30 14:45:48 -0600

    ADDED: Test for token authentication.

--- mygpoclient-java/src/test/java/com/dragontek/mygpoclient/SimpleClientTest.java
+++ mygpoclient-java/src/test/java/com/dragontek/mygpoclient/SimpleClientTest.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Properties;
 
+import org.apache.http.HttpHost;
+
+import com.dragontek.mygpoclient.api.MygPodderClient;
 import com.dragontek.mygpoclient.simple.SimpleClient;
 
 import junit.framework.TestCase;
@@ -17,6 +20,7 @@ public class SimpleClientTest extends TestCase {
 	private String _username;
 	private String _password;
 	private String _deviceId;
+	private String _authToken;
 	
 	public SimpleClientTest(String name) throws FileNotFoundException, IOException {
 		super(name);
@@ -29,14 +33,40 @@ public class SimpleClientTest extends TestCase {
 		_password = properties.getProperty("password");
 		_deviceId = properties.getProperty("deviceId");
 		client = new SimpleClient(_username, _password);
+		
 	}
 
+	public void testLogin() throws Exception
+	{
+		System.out.println(String.format("** %s **", this.getName()) );
+		assertTrue(client.authenticate(_username, _password));
+		_authToken = client.getAuthToken();
+	}
+	
 	public void testGetAuthToken() throws Exception
 	{
 		System.out.println(String.format("** %s **", this.getName()) );
-		String token = client.getAuthToken();
-		assertNotNull(token);
-		System.out.println(token);
+		if(client.authenticate(_username, _password))
+		{
+			String token = client.getAuthToken();
+			System.out.println(token);
+			assertNotNull(token);
+		}
+	}
+	
+	public void testSetAuthToken() throws Exception
+	{
+		String token = null;
+		if(client.authenticate(_username, _password))
+		{
+			token = client.getAuthToken();
+			System.out.println(String.format("TOKEN: %s", token));
+			assertNotNull(token);
+		}		
+		MygPodderClient tempClient = new MygPodderClient(_username);
+		tempClient.setAuthToken(token);
+		tempClient.getSubscriptions(_deviceId);
+		
 	}
 	
 	public void testGetSubscriptions() throws Exception
