5fa6a31b9c1fe2058492a9fbc00f55f16a618167
Josh Mondragon <joshua.mondragon@dragontek.com>
2012-08-17 10:30:31 -0600

    CHANGED: Use new feedservice CHANGED: Only include arguments if they are set

--- mygpoclient-java/src/main/java/com/dragontek/mygpoclient/feeds/FeedServiceClient.java
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/feeds/FeedServiceClient.java
@@ -11,6 +11,7 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.message.BasicNameValuePair;
 
 import com.dragontek.mygpoclient.json.JsonClient;
@@ -19,7 +20,7 @@ import com.google.gson.reflect.TypeToken;
 
 public class FeedServiceClient extends JsonClient {
 
-	private static String HOST="http://mygpo-feedservice.appspot.com";
+	private static String HOST="http://feeds.gpodder.net";
 
 	public FeedServiceClient()
 	{
@@ -39,8 +40,8 @@ public class FeedServiceClient extends JsonClient {
 	@Override
 	public HttpUriRequest prepareRequest(String method, String uri, HttpEntity data) throws UnsupportedEncodingException {
 		HttpUriRequest request = super.prepareRequest(method, uri, data);
+		// TODO: Implement this if-modified-since
 		//request.addHeader("If-Modified-Since", "");
-		//request.addHeader("Accept", "application/json");
 		request.addHeader("Accept-Encoding", "gzip");
 		return request;
 	}
@@ -73,22 +74,31 @@ public class FeedServiceClient extends JsonClient {
 	public FeedServiceResponse parseFeeds(String[] feed_urls, long last_modified, boolean strip_html, boolean use_cache, boolean inline_logo, int scale_logo, String logo_format) throws ClientProtocolException, IOException
 	{
 		List<NameValuePair> args = new ArrayList<NameValuePair>();
-		args.add(new BasicNameValuePair("strip_html", strip_html ? "1" : "0" ));
-		args.add(new BasicNameValuePair("use_cache", use_cache ? "1" : "0" ));
-		args.add(new BasicNameValuePair("inline_logo", inline_logo ? "1" : "0" ));
-		args.add(new BasicNameValuePair("scale_logo", Integer.toString(scale_logo)));
-		args.add(new BasicNameValuePair("logo_format", logo_format));
+		if(strip_html)
+			args.add(new BasicNameValuePair("process_text", "strip_html"));
+		if(use_cache)
+			args.add(new BasicNameValuePair("use_cache", "1"));
+		if(inline_logo)
+		{
+			args.add(new BasicNameValuePair("inline_logo", "1"));
+			args.add(new BasicNameValuePair("scale_logo", Integer.toString(scale_logo)));
+		}
+		if(logo_format != null && !logo_format.isEmpty())
+			args.add(new BasicNameValuePair("logo_format", logo_format));
+
 		for(String feed_url : feed_urls)
 		{
 			args.add(new BasicNameValuePair("url", feed_url));
 		}
 		
-		UrlEncodedFormEntity post_data = new UrlEncodedFormEntity(args, "UTF-8");
-		
 		Gson gson = new Gson();
 		Type collectionType = new TypeToken<ArrayList<Feed>>(){}.getType();
-		List<Feed> response = gson.fromJson(this.POST(HOST + "/parse" , post_data), collectionType);
-		
+
+		// POST
+		List<Feed> response = gson.fromJson(this.POST(HOST + "/parse", new UrlEncodedFormEntity(args, "UTF-8")), collectionType);
+		// GET
+		//List<Feed> response = gson.fromJson(this.GET(HOST + "/parse?" + URLEncodedUtils.format(args, "UTF-8")), collectionType);	
+
 		return new FeedServiceResponse(response, 0L, feed_urls);
 	}
 	
