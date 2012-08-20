8a4a3ce9950296c2e838d125d74836af2d56999a
Josh Mondragon <joshua.mondragon@dragontek.com>
2012-08-20 14:41:09 -0600

    REMOVED: Extra python comments

--- mygpoclient-java/src/main/java/com/dragontek/mygpoclient/feeds/FeedServiceResponse.java
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/feeds/FeedServiceResponse.java
@@ -6,34 +6,6 @@ import java.util.List;
 @SuppressWarnings("serial")
 public class FeedServiceResponse extends ArrayList<Feed> {
 
-	/*
-	 *     def __init__(self, feeds, last_modified, feed_urls):
-        super(FeedServiceResponse, self).__init__(feeds)
-        self.last_modified = last_modified
-        self.feed_urls = feed_urls
-        self.indexed_feeds = {}
-        for feed in feeds:
-            for url in feed['urls']:
-                self.indexed_feeds[url] = feed
-
-
-    def get_feeds(self):
-        """
-        Returns the parsed feeds in order of the initial request
-        """
-        return (self.get_feed(url) for url in self.feed_urls)
-
-
-    def get_feed(self, url):
-        """
-        Returns the parsed feed for the given URL
-        """
-        return self.indexed_feeds.get(url, None)
-	 */
-	
-	/**
-	 * 
-	 */
 	public long last_modfied;
 	public String[] feed_urls;
 	public String[] indexed_feeds = {};
