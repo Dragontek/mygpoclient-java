e1c91cee5633db1b993da74972cda6f66fd09fbb
Josh Mondragon <joshua.mondragon@dragontek.com>
2012-08-20 14:39:19 -0600

    ADDED: Implements IFeed

--- mygpoclient-java/src/main/java/com/dragontek/mygpoclient/feeds/Feed.java
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/feeds/Feed.java
@@ -3,41 +3,123 @@ package com.dragontek.mygpoclient.feeds;
 import java.util.Dictionary;
 import java.util.List;
 
-public class Feed {
-	public String title;
-	public String link;
-	public String description;
-	public String author;
-	public String language;
-	public String[] urls;
-	public String new_location;
-	public String logo;
-	public String logo_data;
-	public String[] content_types;
-	public String hub;
-	public Dictionary<String, String> errors;
-	public Dictionary<String, String> warnings;
-	public long http_last_modified;
-	public String http_etag;
-	public Episode[] episodes;
-	
-	public class Episode {
-		public String guid;
-		public String title;
-		public String short_title;
-		public String number;
-		public String description;
-		public String link;
-		public long released;
-		public String author;
-		public long duration;
-		public String language;
-		public Enclosure[] files;
+public class Feed implements IFeed {
+	private String title;
+	private String link;
+	private String description;
+	private String author;
+	private String language;
+	private String[] urls;
+	private String new_location;
+	private String logo;
+	private String logo_data;
+	private String[] content_types;
+	private String hub;
+	private Dictionary<String, String> errors;
+	private Dictionary<String, String> warnings;
+	private long http_last_modified;
+	private String http_etag;
+	private Episode[] episodes;
+	public String getTitle()
+	{
+		return this.title;
+	}
+	public String getDescription()
+	{
+		return this.description;
+	}
+	public String getLink()
+	{
+		return this.link;
+	}
+	public String getUrl()
+	{
+		return this.urls[0];
+	}
+	public String getLogoUrl()
+	{
+		return this.logo;
+	}
+	public IEpisode[] getEpisodes()
+	{
+		return this.episodes;
+	}
+	public class Episode implements IEpisode {
+		private String guid;
+		private String title;
+		private String short_title;
+		private String number;
+		private String description;
+		private String link;
+		private long released;
+		private String author;
+		private long duration;
+		private String language;
+		private Enclosure[] files;
+
+		public IEnclosure getEnclosure(){
+			return files[0];
+		}
 		
-		public class Enclosure {
-			public String url;
-			public String mimetype;
-			public long filesize;
+		public class Enclosure implements IEnclosure {
+			private String url;
+			private String[] urls;
+			private String mimetype;
+			private long filesize;
+			
+			@Override
+			public String getUrl() {
+				if(url != null)
+					return url;
+				else
+					return urls[0];
+			}
+			@Override
+			public String getMimetype() {
+				return mimetype;
+			}
+			@Override
+			public long getFilesize() {
+				return filesize;
+			}
+		}
+
+
+		@Override
+		public String getGuid() {
+			return guid;
+		}
+		@Override
+		public String getTitle() {
+			return title;
+		}
+		@Override
+		public String getDescription() {
+			return description;
+		}
+		@Override
+		public String getLink() {
+			return link;
+		}
+		@Override
+		public long getReleased() {
+			return released;
+		}
+
+		public String getNumber() {
+			return number;
+		}
+		public String getShort_title() {
+			return short_title;
+		}
+		public String getAuthor() {
+			return author;
+		}
+		public long getDuration() {
+			return duration;
+		}
+		public String getLanguage() {
+			return language;
 		}
 	}
 }
