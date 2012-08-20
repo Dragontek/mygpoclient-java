ab911f674baa62092fb7c96da9c7340191a66976
Josh Mondragon <joshua.mondragon@dragontek.com>
2012-08-20 14:40:44 -0600

    CHANGED: IFeed interface simplified

--- mygpoclient-java/src/main/java/com/dragontek/mygpoclient/feeds/IFeed.java
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/feeds/IFeed.java
@@ -1,28 +1,39 @@
 package com.dragontek.mygpoclient.feeds;
 
-
+/**
+ * A standard interface that is used for all Feed classes
+ * 
+ * @author jmondragon
+ *
+ */
 // TODO: Determine if we need setters since these feeds
 //		will be populated via Gson
 public interface IFeed {
 
 	public String getTitle();
-	public String[] getUrls();
+	public String getUrl();
 	public String getDescription();
 	public String getLink();
-	public String getLogoUrl();
+	// I would like to do this, but not all feeds include logo
+	//public String getLogoUrl();
 	public IEpisode[] getEpisodes();
 	
 	public interface IEpisode
 	{
+		public String getGuid();
 		public String getTitle();
 		public String getDescription();
-		public IEnclosure[] getEnclosures();
+		public long getReleased();
+		public String getLink();
+		public String getAuthor();
 		
+		public IEnclosure getEnclosure();
+
 		public interface IEnclosure
 		{
-			public String getURL();
-			public String getMimeType();
-			public Long getLength();
+			public String getUrl();
+			public String getMimetype();
+			public long getFilesize();
 		}
 	}
 }
