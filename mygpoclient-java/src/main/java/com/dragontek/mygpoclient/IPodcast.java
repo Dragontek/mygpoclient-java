ccd2acf4373b95a3e939e94eecbd960ef5fa0026
Josh Mondragon <joshua.mondragon@dragontek.com>
2012-08-20 14:37:58 -0600

    ADDED: IPodcast interface

---
+++ mygpoclient-java/src/main/java/com/dragontek/mygpoclient/IPodcast.java
@@ -0,0 +1,11 @@
+package com.dragontek.mygpoclient;
+
+public interface IPodcast {
+	public String getUrl();
+	public String getTitle();
+	public void setTitle(String title);
+	public String getDescription();
+	public void setDescription(String description);
+	public String getLogoUrl();
+	public void setLogoUrl(String logourl);
+}
